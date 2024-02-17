5bcef4c8745ae3e79d315375c9281f6eca1431f2
sameerman <shresthasamerman@gmail.com>
2024-02-17 20:00:29 +0545

    Add project files.

---
+++ Simple_calculator/Simple_calculator.py
@@ -0,0 +1,28 @@
+def add(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+def truncated_divide(x,y):
+    return x//y
+def modulus(x,y):
+    return x%y
+def exponentiate(x,y):
+    return x**y
+addition = add(2.5,3.6)
+substraction = substract(10.21,2.90)
+multiplication = multiply(3.1,6.4)
+division=divide(12.0,4.0)
+truncated_division=truncated_divide(15,2)
+modulus=modulus(13,5)
+exponentiation=exponentiate(2,8)
+print(f"Addition: {addition}")
+print(f"Subtraction: {substraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+print(f"Truncated Division: {truncated_division}")
+print(f"Modulus: {modulus}")
+print(f"Exponentiation: {exponentiation}")
